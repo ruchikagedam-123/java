d100c4d7f3cfbce113b3f22e6a281942f082c5c9
ruchikagedam-123 <67160652+ruchikagedam-123@users.noreply.github.com>
2020-06-30 17:42:17 +0530

    Java regex - removing duplicates

--- removing_duplicate.java
+++ removing_duplicate.java
@@ -44,3 +44,24 @@ public class DuplicateWords {
         in.close();
     }
 }
+
+
+Output :
+
+Sample Input
+
+5
+Goodbye bye bye world world world
+Sam went went to to to his business
+Reya is is the the best player in eye eye game
+in inthe
+Hello hello Ab aB
+
+
+Sample Output
+
+Goodbye bye world
+Sam went to his business
+Reya is the best player in eye game
+in inthe
+Hello Ab
